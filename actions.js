db488e6b555d022762cd0eb39ba930cd8e2f5c87
Alex McPherson <alex.mcpherson@worldpay.com>
2021-12-02 13:47:28 +0000

    test

--- actions.js
+++ actions.js
@@ -166,4 +166,51 @@ $(function() {
     $("#sendActionCommandBtn").click( function() {
         sendMovementCommand();
     });
-});
+
+    // DPAD
+    $("#d-up").click( function(e) {
+        e.preventDefault();
+        logAction("d-up");
+    });
+
+    $("#d-down").click( function(e) {
+        e.preventDefault();
+        logAction("d-down");
+    });
+
+    $("#d-left").click( function(e) {
+        e.preventDefault();
+        logAction("d-left");
+    });
+
+    $("#d-right").click( function(e) {
+        e.preventDefault();
+        logAction("d-right");
+    });
+
+    // OPAD
+    $("#o-up").click( function(e) {
+        e.preventDefault();
+        logAction("o-up");
+    });
+
+    $("#o-down").click( function(e) {
+        e.preventDefault();
+        logAction("o-down");
+    });
+
+    $("#o-left").click( function(e) {
+        e.preventDefault();
+        logAction("o-left");
+    });
+
+    $("#o-right").click( function(e) {
+        e.preventDefault();
+        logAction("o-right");
+    });
+
+    function logAction(action) {
+        console.log(action);
+    }
+    
+    });
