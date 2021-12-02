d96eb3cf02ce1b7470745431e3da5037bb949cba
Alex McPherson <alex.mcpherson@worldpay.com>
2021-12-02 12:59:15 +0000

    Actions

--- dpad.js
+++ dpad.js
@@ -1,5 +1,41 @@
 $(function() {
 
-  // TODO DPAD actions here
+  // DPAD
+  $("#d-up").click( function() {
+        logAction("d-up");
+  });
+
+  $("#d-down").click( function() {
+        logAction("d-down");
+  });
+
+  $("#d-left").click( function() {
+        logAction("d-left");
+  });
+
+  $("#d-right").click( function() {
+        logAction("d-right");
+  });
+
+  // OPAD
+  $("#o-up").click( function() {
+        logAction("o-up");
+  });
+
+  $("#o-down").click( function() {
+        logAction("o-down");
+  });
+
+  $("#o-left").click( function() {
+        logAction("o-left");
+  });
+
+  $("#o-right").click( function() {
+        logAction("o-right");
+  });
+
+  fucntion logAction(action) {
+    console.log(action)
+  }
 
 });
