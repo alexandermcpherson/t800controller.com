99a846659571001d6d41b9d584e9d946971d1c81
Alex McPherson <alex.mcpherson@worldpay.com>
2021-12-04 16:27:33 +0000

    send correct command

--- dpad.js
+++ dpad.js
@@ -97,7 +97,7 @@ $(function() {
     e.preventDefault();
     logAction("o-left");
 
-    if (eyesLeftAndRight < EYESMAX && eyesLeftAndRight != 0) {
+    if (eyesLeftAndRight < EYESMAX && eyesLeftAndRight >= EYESMIN) {
       eyesLeftAndRight -=1;
       var command = "S_" + eyesLeftAndRight + "_E";
       console.log(command);
@@ -109,7 +109,7 @@ $(function() {
     e.preventDefault();
     logAction("o-right");
 
-    if (eyesLeftAndRight > EYESMIN) {
+    if (eyesLeftAndRight > EYESMIN && eyesLeftAndRight <= EYESMAX) {
       eyesLeftAndRight += 1;
       var command = "S_" + eyesLeftAndRight + "_E";
       console.log(command);
