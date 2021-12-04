0c49864ab5f42ac5c065acd937b0483959073070
Alex McPherson <alex.mcpherson@worldpay.com>
2021-12-04 15:29:38 +0000

    Added jaw commands for o-pad

--- dpad.js
+++ dpad.js
@@ -7,9 +7,13 @@ const HEADXMAX = 160;
 const EYESMIN = 0;
 const EYESMAX = 180;
 
+const JAWMIN = 0;
+const JAWMAX = 40;
+
 var headUpAndDown = 0;
 var headLeftAndRight = 90;
 var eyesLeftAndRight = 90;
+var jawUpAndDown = 0;
 
 $(function() {
 
@@ -22,7 +26,7 @@ $(function() {
       headUpAndDown += 5;
       var command = "S_" + headUpAndDown + "_N";
       console.log(command);
-      sendHeadLeftAndRightCommand(command);
+      sendCommand(command);
     }
   });
 
@@ -35,7 +39,7 @@ $(function() {
       headUpAndDown -= 5;
       var command = "S_" + headUpAndDown + "_N";
       console.log(command);
-      sendHeadLeftAndRightCommand(command);
+      sendCommand(command);
     }
   });
 
@@ -47,7 +51,7 @@ $(function() {
       headLeftAndRight -= 5;
       var command = "S_" + headLeftAndRight + "_H";
       console.log(command);
-      sendHeadLeftAndRightCommand(command);
+      sendCommand(command);
     }
 
   });
@@ -55,44 +59,73 @@ $(function() {
   $("#d-right").click( function(e) {
     e.preventDefault();
     logAction("d-right");
-    
+
     if (headLeftAndRight < HEADXMAX) {
       headLeftAndRight += 5;
       var command = "S_" + headLeftAndRight + "_H";
       console.log(command);
-      sendHeadLeftAndRightCommand(command);
+      sendCommand(command);
     }
   });
 
   // OPAD
   $("#o-up").click( function(e) {
     e.preventDefault();
-    logAction("o-up");
+    logAction("0-up");
+
+    if (jawUpAndDown < JAWMAX) {
+      jawUpAndDown += 1;
+      var command = "S_" + jawUpAndDown + "_J";
+      console.log(command);
+      sendCommand(command);
+    }
   });
 
   $("#o-down").click( function(e) {
     e.preventDefault();
     logAction("o-down");
+
+    if (jawUpAndDown > JAWMIN) {
+      jawUpAndDown -= 1;
+      var command = "S_" + headUpAndDown + "_J";
+      console.log(command);
+      sendCommand(command);
+    }
   });
 
   $("#o-left").click( function(e) {
     e.preventDefault();
     logAction("o-left");
+
+    if (eyesLeftAndRight > EYESMIN) {
+      eyesLeftAndRight -= 1;
+      var command = "S_" + headLeftAndRight + "_E";
+      console.log(command);
+      sendCommand(command);
+    }
   });
 
   $("#o-right").click( function(e) {
     e.preventDefault();
     logAction("o-right");
+
+    if (eyesLeftAndRight < EYESMAX) {
+      eyesLeftAndRight += 1;
+      var command = "S_" + headLeftAndRight + "_E";
+      console.log(command);
+      sendCommand(command);
+    }
   });
 
   function logAction(action) {
     console.log(action);
   }
 
-  function sendHeadLeftAndRightCommand(command) {
+  function sendHCommand(command) {
     const encoder = new TextEncoder('utf-8');
     const commandToSend = encoder.encode(command);
     myCharateristic.writeValueWithoutResponse(commandToSend);
   }
+
 
 });
